d3f1d18dbe711b6eebcfa0bc123483c7bbed3fe1
Yunah Kim <yunah.kim@stonybrook.edu>
2023-09-04 14:43:05 -0400

    input form for uploading data

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import "./App.css";
 
 function App() {
   return (
-    <Map
-      mapboxAccessToken="pk.eyJ1IjoieXVuYWhraW0iLCJhIjoiY2xtNTgybXd2MHdtMjNybnh6bXYweGNweiJ9.cfBakJXxub4ejba076E2Cw"
-      initialViewState={{
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 14,
-      }}
-      style={{ width: 600, height: 400 }}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-    />
+    <>
+      <form>
+        <input type="file" />
+      </form>
+      <Map
+        mapboxAccessToken="pk.eyJ1IjoieXVuYWhraW0iLCJhIjoiY2xtNTgybXd2MHdtMjNybnh6bXYweGNweiJ9.cfBakJXxub4ejba076E2Cw"
+        initialViewState={{
+          longitude: -122.4,
+          latitude: 37.8,
+          zoom: 14,
+        }}
+        style={{ width: 600, height: 400 }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      />
+    </>
   );
 }
 
