3bc4f9a8cf692d92d103327696818c980d695ab9
Yunah Kim <yunah.kim@stonybrook.edu>
2023-09-04 15:05:24 -0400

    merge

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+/* import * as React from "react";
 import Map from "react-map-gl";
 import "./App.css";
 
@@ -20,6 +20,49 @@ function App() {
       />
     </>
   );
+}
+
+export default App; */
+
+import React, { Component } from "react";
+import ReactMapboxGL, { Source, Layer } from "react-map-gl";
+import './App.css'
+
+class App extends Component {
+  state = {
+    viewport: {
+      width: "100vw",
+      height: "90vh",
+      latitude: 44.065256,
+      longitude: -125.075801,
+      zoom: 3,
+    },
+  };
+  render() {
+    return (
+      <div className="Mapbox">
+        <ReactMapboxGL
+          {...this.state.viewport}
+          onViewportChange={(viewport) => this.setState({ viewport })}
+          mapStyle="mapbox://styles/mapbox/outdoors-v11"
+          /* mapboxApiAccessToken="pk.eyJ1IjoieXVuYWhraW0iLCJhIjoiY2xtNTgybXd2MHdtMjNybnh6bXYweGNweiJ9.cfBakJXxub4ejba076E2Cw" */
+          mapboxAccessToken="pk.eyJ1IjoieXVuYWhraW0iLCJhIjoiY2xtNTgybXd2MHdtMjNybnh6bXYweGNweiJ9.cfBakJXxub4ejba076E2Cw"
+        >
+          <Source
+            id="oregonjson"
+            type="geojson"
+            data="https://raw.githubusercontent.com/glynnbird/usstatesgeojson/master/california.geojson"
+          />
+          <Layer
+            id="anything"
+            type="fill"
+            source="oregonjson"
+            paint={{ "fill-color": "#228b22", "fill-opacity": 0.4 }}
+          />
+        </ReactMapboxGL>
+      </div>
+    );
+  }
 }
 
 export default App;
