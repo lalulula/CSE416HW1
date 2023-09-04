79bff7ff51b58f27588bee45c927cbfd1ad08ccc
Yunah Kim <yunah.kim@stonybrook.edu>
2023-09-04 14:40:47 -0400

    render map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from "./logo.svg";
+import * as React from "react";
+import Map from "react-map-gl";
 import "./App.css";
 
 function App() {
   return (
-    <form>
-      <input type="file" />
-    </form>
+    <Map
+      mapboxAccessToken="pk.eyJ1IjoieXVuYWhraW0iLCJhIjoiY2xtNTgybXd2MHdtMjNybnh6bXYweGNweiJ9.cfBakJXxub4ejba076E2Cw"
+      initialViewState={{
+        longitude: -122.4,
+        latitude: 37.8,
+        zoom: 14,
+      }}
+      style={{ width: 600, height: 400 }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    />
   );
 }
 
